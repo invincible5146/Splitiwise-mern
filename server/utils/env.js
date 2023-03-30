3dd3fe9c3d3dac9389a43a40c308cfd9d0bd1a39
sachintiwari909 <sachin.tiwari@awign.com>
2023-03-30 13:43:36 +0530

    Splitwise full stack done

---
+++ server/utils/env.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongo: 'mongodb://sachintiwari057:lH6iM6Cxh89tM7FM@ac-uvt6lgw-shard-00-00.qi5qnzd.mongodb.net:27017,ac-uvt6lgw-shard-00-01.qi5qnzd.mongodb.net:27017,ac-uvt6lgw-shard-00-02.qi5qnzd.mongodb.net:27017/?ssl=true&replicaSet=atlas-oyosx7-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
